4fd303a10b54b7ad65b454c9e2f223683fbe9f3c
TripleK2004 <triplek1862004@gmail.com>
2022-05-27 06:12:01 +0530

    hardcoded gaps to monocle lt

--- dwm.c
+++ dwm.c
@@ -1401,7 +1401,7 @@ monocle(Monitor *m)
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		resize(c, m->wx + 7.5, m->wy + 7.5, m->ww - 2 * c->bw - 2 * 7.5, m->wh - 2 * c->bw - 2 * 7.5, 0);
 }
 
 void
