6133f2c5889dcb2e29197ea38fe36a23dcc346d2
maki40681 <maki4002681@gmail.com>
2023-11-14 23:13:28 +0530

    update bar height patch v6.3

--- dwm.c
+++ dwm.c
@@ -2056,7 +2056,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = user_bh ? user_bh : drw->fonts->h + 2;
+	bh = drw->fonts->h + user_bh;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
